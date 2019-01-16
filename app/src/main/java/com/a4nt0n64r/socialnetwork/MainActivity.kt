119b17d961862abe93344363feeca1b29844388f
Anton Sharapov <cheel4069@gmail.com>
2019-01-16 21:02:51 +0200

    fix androidX, why test != test?

--- app/src/main/java/com/a4nt0n64r/socialnetwork/MainActivity.kt
+++ app/src/main/java/com/a4nt0n64r/socialnetwork/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.a4nt0n64r.socialnetwork
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
+//import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,30 +16,38 @@ class MainActivity : AppCompatActivity() {
 
         fun isFieldEmpty(field: EditText) = field.text.isEmpty()
         fun isCredentialsCorrect(login: EditText, passwd: EditText): Boolean {
-            return (login.text.toString() == R.string.credentials.toString()) &&
-                    (passwd.text.toString() == R.string.credentials.toString())
+            Log.d("STR",(login.text.toString() == R.string.credentials.toString()).toString())
+            Log.d("STR",passwd.text.toString())
+            return ((login.text.toString() == R.string.credentials.toString()) and
+                    (passwd.text.toString() == R.string.credentials.toString()))
         }
 
         login_button.setOnClickListener {
-            if (isFieldEmpty(login_textView) && !isFieldEmpty(passwd_textView)) {
+            if (isFieldEmpty(login_textView) and !isFieldEmpty(passwd_textView)) {
                 Snackbar.make(
                     constr,
                     R.string.empty_login,
                     Snackbar.LENGTH_SHORT
                 ).show()
-            } else if (!isFieldEmpty(login_textView) && isFieldEmpty(passwd_textView)) {
+            } else if (!isFieldEmpty(login_textView) and isFieldEmpty(passwd_textView)) {
                 Snackbar.make(
                     constr,
                     R.string.empty_password,
                     Snackbar.LENGTH_SHORT
                 ).show()
-            } else if (isFieldEmpty(login_textView) && isFieldEmpty(passwd_textView)) {
+            } else if (isFieldEmpty(login_textView) and isFieldEmpty(passwd_textView)) {
                 Snackbar.make(
                     constr,
                     R.string.empty_data,
                     Snackbar.LENGTH_SHORT
                 ).show()
-            } else if (isCredentialsCorrect(login_textView,passwd_textView)) {
+            } else if (!isCredentialsCorrect(login_textView,passwd_textView)) {
+                Snackbar.make(
+                    constr,
+                    R.string.incorrect_credentials,
+                    Snackbar.LENGTH_SHORT
+                ).show()
+            }else{
                 //старт news activity
             }
         }
